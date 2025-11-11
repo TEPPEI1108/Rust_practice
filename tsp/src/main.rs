1c515ca9d457dde5ff6574c19ee7bd3c517cf7e3
TEPPEI1108 <fepeacet@gmail.com>
2025-11-11 22:34:02 +0900

    tsp challenge from NEC PC

--- tsp/src/main.rs
+++ tsp/src/main.rs
@@ -1,6 +1,20 @@
 use std::f64:INFINITY;
 
 fn held_karp(dist: &Vec<Vec<f64>>) -> (f64, Vec<size>)  {
+    let n = dist.len();
+    assert!(n >= 1);
+    if(n == 1) {
+        return (0.0, vec![0, 0]);
+    }
+
+    let full_mask =  (1usize << n) - 1;
+    let size = 1usize << n;
+
+    let mut dp = vec![INFINITY; size * n];
+    let mut parent = vec![usize::MAX; size * n];
+
+    let start_mask = 1usize << 0;
+    dp[start_mask * n + 0] = 0.0;
     
 }
 
